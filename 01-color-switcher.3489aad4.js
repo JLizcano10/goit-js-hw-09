ecec870f141b396dabfb9381c68963ea360adefa
JLizcano10 <126992358+JLizcano10@users.noreply.github.com>
2023-09-26 02:21:46 +0000

    Deploying to gh-pages from @ JLizcano10/goit-js-hw-09@7bac7e0c65cc19ccdbf82872b841fa63c0525dfb 🚀

--- 01-color-switcher.3489aad4.js
+++
@@ -1,2 +0,0 @@
-const t=document.body,e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");let d;e.addEventListener("click",(function(){d=setInterval((()=>{sortColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,t.style.backgroundColor=sortColor,e.disabled=!0,o.disabled=!1}),1e3)})),o.addEventListener("click",(function(){o.disabled=!0,e.disabled=!1,clearInterval(d)}));
-//# sourceMappingURL=01-color-switcher.3489aad4.js.map
