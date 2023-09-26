f7e6ffdac6b36cca479c917ea1589d649f066398
JL10 <126992358+JLizcano10@users.noreply.github.com>
2023-09-25 21:15:43 -0500

    task01-colorChange Complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const body = document.body;
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let colorInterval;
+const intervalDuration = 1000;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeColorInterval() {
+  colorInterval = setInterval(() => {
+    sortColor = getRandomHexColor();
+    body.style.backgroundColor = sortColor;
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  }, intervalDuration);
+}
+
+function stopColorInterval() {
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+  clearInterval(colorInterval);
+}
+
+startBtn.addEventListener('click', changeColorInterval);
+stopBtn.addEventListener('click', stopColorInterval);
