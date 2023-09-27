4a0671bea007cf84cc6aac5672e273a88c9f48f0
JL10 <126992358+JLizcano10@users.noreply.github.com>
2023-09-26 19:07:10 -0500

    install and first usage flatpickr task 02- timer

---
+++ src/js/02-timer.js
@@ -0,0 +1,17 @@
+// Descrito en la documentación
+import flatpickr from 'flatpickr';
+// Importación adicional de estilos
+import 'flatpickr/dist/flatpickr.min.css';
+
+const inputTimePicker = document.querySelector('#datetime-picker');
+const startTimeBtn = document.querySelector('button[data-start]');
+
+flatpickr('input', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+});
