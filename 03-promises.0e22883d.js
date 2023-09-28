782fdbe495d365d5f42c37b0b027dd8530663dd3
JLizcano10 <126992358+JLizcano10@users.noreply.github.com>
2023-09-28 21:55:05 +0000

    Deploying to gh-pages from @ JLizcano10/goit-js-hw-09@b24dcd6e0f3548af8c9b3648ab90e72ca04993cf 🚀

--- 03-promises.0e22883d.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form"),document.querySelector('input[name= "delay"]'),document.querySelector('input[name= "step"]'),document.querySelector('input[name= "amount"]'),document.querySelector('button[type = "submit"]');
-//# sourceMappingURL=03-promises.0e22883d.js.map
