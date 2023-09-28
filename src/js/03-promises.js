b24dcd6e0f3548af8c9b3648ab90e72ca04993cf
JL10 <126992358+JLizcano10@users.noreply.github.com>
2023-09-28 16:54:34 -0500

    add createPromise function of GOIT

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 const promiseForm = document.querySelector('.form');
 const delayInput = document.querySelector('input[name= "delay"]');
 const stepInput = document.querySelector('input[name= "step"]');
@@ -5,6 +7,9 @@ const amountInput = document.querySelector('input[name= "amount"]');
 
 const submitBtn = document.querySelector('button[type = "submit"]');
 
+promiseForm.addEventListener('submit', e => {
+  Notiflix.Notify.success(e.target);
+});
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -13,3 +18,11 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
