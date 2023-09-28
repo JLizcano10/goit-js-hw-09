2605cdf125aad762a21585d38319907b862d0fd1
JL10 <126992358+JLizcano10@users.noreply.github.com>
2023-09-27 23:54:16 -0500

    task 03-promises first querySelectors

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,10 @@
+const promiseForm = document.querySelector('.form');
+const delayInput = document.querySelector('input[name= "delay"]');
+const stepInput = document.querySelector('input[name= "step"]');
+const amountInput = document.querySelector('input[name= "amount"]');
+
+const submitBtn = document.querySelector('button[type = "submit"]');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
